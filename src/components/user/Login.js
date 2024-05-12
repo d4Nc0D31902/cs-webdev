6b7e5cbba570917e7b1bbc9277476778f9f36f63
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-05-12 20:27:15 +0800

    Login & ICO

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,99 +1,164 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-
-import Loader from '../layout/Loader'
-import MetaData from '../layout/MetaData'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import { useDispatch, useSelector } from 'react-redux'
-import { login, clearErrors } from '../../actions/userActions'
+import React, { Fragment, useState, useEffect } from "react";
+import { Link as RouterLink, useNavigate, useLocation } from "react-router-dom";
+import {
+  Divider,
+  TextField,
+  Button,
+  Link,
+  Typography,
+  Grid,
+} from "@mui/material";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login, clearErrors } from "../../actions/userActions";
+import Loader from "../layout/Loader";
+import MetaData from "../layout/MetaData";
 
 const Login = () => {
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const dispatch = useDispatch();
-    const navigate = useNavigate()
-    let location = useLocation();
-    const { isAuthenticated, error, loading } = useSelector(state => state.auth);
-    // const redirect = location.search ? location.search.split('=')[1] : ''
-    const redirect = new URLSearchParams(location.search).get('redirect')
-    const notify = (error = '') => toast.error(error, {
-        position: toast.POSITION.BOTTOM_CENTER
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  let location = useLocation();
+  const { isAuthenticated, error, loading } = useSelector(
+    (state) => state.auth
+  );
+  const redirect = new URLSearchParams(location.search).get("redirect");
+  const notify = (error = "") =>
+    toast.error(error, {
+      position: toast.POSITION.BOTTOM_CENTER,
     });
-    useEffect(() => {
-        if (isAuthenticated && redirect === 'shipping') {
-            navigate(`/${redirect}`, { replace: true })
-        }
-        else if (isAuthenticated)
-            navigate('/')
-        if (error) {
-            // alert.error(error);
-            console.log(error)
-            notify(error)
-            dispatch(clearErrors());
-        }
-
-    }, [dispatch, isAuthenticated, error, navigate, redirect])
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(login(email, password))
+  useEffect(() => {
+    if (isAuthenticated && redirect === "shipping") {
+      navigate(`/${redirect}`, { replace: true });
+    } else if (isAuthenticated) navigate("/");
+    if (error) {
+      console.log(error);
+      notify(error);
+      dispatch(clearErrors());
     }
+  }, [dispatch, isAuthenticated, error, navigate, redirect]);
 
-    return (
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? <Loader /> : (
-                <Fragment>
-                    <MetaData title={'Login'} />
+          <MetaData title={"Login"} />
+          <Grid
+            container
+            justifyContent="center"
+            sx={{
+                // border: "2px white solid",
+              marginTop: "20px",
+              height: "100vh",
+            }}
+          >
+            <Grid
+              item
+              xs={10}
+              lg={5}
+              sx={{
+                alignContent: "center",
+              }}
+            >
+              <form
+                style={{
+                  border: "2px solid white",
+                  //   backgroundColor: "white",
+                  margin: "20px",
+                  padding: "20px",
+                  height: "480px",
+                  borderRadius: "50px",
+                }}
+                className="shadow-lg"
+                onSubmit={submitHandler}
+              >
+                <Typography
+                  sx={{ color: "white", textAlign: "center" }}
+                  variant="h2"
+                  gutterBottom
+                >
+                  Login
+                </Typography>
+                <TextField
+                  id="email_field"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  margin="normal"
+                  focused
+                  InputProps={{
+                    style: { color: "white" },
+                  }}
+                  InputLabelProps={{
+                    style: { color: "white" },
+                  }}
+                />
 
-                    <div className="row wrapper">
-                        <div className="col-10 col-lg-5">
-                            <form className="shadow-lg" onSubmit={submitHandler}>
-                                <h1 className="mb-3">Login</h1>
-                                <div className="form-group">
-                                    <label htmlFor="email_field">Email</label>
-                                    <input
-                                        type="email"
-                                        id="email_field"
-                                        className="form-control"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </div>
+                <TextField
+                  id="password_field"
+                  label="Password"
+                  type="password"
+                  focused
+                  fullWidth
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  margin="normal"
+                  InputProps={{
+                    style: { color: "white" },
+                  }}
+                  InputLabelProps={{
+                    style: { color: "white" },
+                  }}
+                />
 
-                                <div className="form-group">
-                                    <label htmlFor="password_field">Password</label>
-                                    <input
-                                        type="password"
-                                        id="password_field"
-                                        className="form-control"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </div>
-
-                                <Link to="/password/forgot" className="float-right mb-4">Forgot Password?</Link>
-
-                                <button
-                                    id="login_button"
-                                    type="submit"
-                                    className="btn btn-block py-3"
-                                >
-                                    LOGIN
-                                </button>
-
-                                <Link to="/register" className="float-right mt-3">New User?</Link>
-                            </form>
-                        </div>
-                    </div>
-
-
-                </Fragment>
-            )}
+                <Link
+                  component={RouterLink}
+                  to="/password/forgot"
+                  variant="body2"
+                  className="float-right mb-4"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Forgot Password?
+                </Link>
+                <Button
+                  id="login_button"
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  size="large"
+                  className="py-3"
+                  sx={{ borderRadius: "20px" }}
+                >
+                  LOGIN
+                </Button>
+                <Link
+                  component={RouterLink}
+                  to="/register"
+                  variant="body2"
+                  style={{ textDecoration: "none", color: "white" }}
+                  className="float-right mt-3"
+                >
+                  New User?
+                </Link>
+              </form>
+            </Grid>
+          </Grid>
         </Fragment>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
-export default Login
+export default Login;
